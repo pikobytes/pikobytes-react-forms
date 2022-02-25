ec1beb61986bfb3fac12da31e6bb4a0e81de2542
Jacob Mendt <jacob.mendt@pikobytes.de>
2022-02-25 10:17:55 +0100

    Remove unncessary dependencies nad code

--- lib/Form/components/Inputs/DatePicker/DatePicker.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="react" />
-import { IDefaultUiSettings, IGenericField } from '../../../typedefs/IField';
-export declare function DatePicker({ fieldId, uiSettings: { disabled, description, label, placeholder, size, variant, }, validation, }: IGenericField<IDefaultUiSettings, undefined>): JSX.Element;
-export default DatePicker;
