aec8a70e3e3eb0708c6ad7442e58b17bcded3626
Nicolas Looschen <nicolas.looschen@pikobytes.de>
2022-07-06 21:19:11 +0200

    Fix license header

--- src/components/Inputs/Autocomplete/Autocomplete.stories.tsx
+++ src/components/Inputs/Autocomplete/Autocomplete.stories.tsx
@@ -1,5 +1,5 @@
 /**
- * Created by nicolas.looschen@pikobytes.de on 01.02.22
+ * Created by nicolas.looschen@pikobytes.de on 06.07.22
  *
  * This file is subject to the terms and conditions defined in
  * file 'LICENSE.txt', which is part of this source code package.
